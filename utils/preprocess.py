4ea9f4452f949ff54b0af6dfdb5e195feb0faa08
“Vansh <vanshgulati110@gmail.com>
2026-04-13 02:10:41 +0200

    remove spacy for deployment fix

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,9 +1,3 @@
-import spacy
-
-# load NLP model
-nlp = spacy.load("en_core_web_sm")
-
-# predefined skills list (you can expand later)
 SKILLS = [
     "python", "machine learning", "data science",
     "deep learning", "pytorch", "tensorflow",
